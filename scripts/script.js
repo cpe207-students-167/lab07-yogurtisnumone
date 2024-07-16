62be610d8ae7f3b0a14ee0bf87aec1021ed993ce
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-07-17 01:02:54 +0700

    yooo!

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const confirmPasswordInput = document.querySelector("#password-confirm-input");
 
 // create reference for buttons.
 const submitBtn = document.querySelector("#submit-btn");
-const clearBtn = document.querySelector("#clear-btn");
+const clearBtn = document.querySelector("#reset-btn");
 
 // simple email validation
 function validateEmail(email) {
@@ -24,6 +24,22 @@ firstNameInput.onkeyup = () => {
 
 // add callback functions for other input events.
 // (lastname, email, password, confirm password)
+lastNameInput.onkeyup = () => {
+  clearValidationStyles(lastNameInput);
+};
+
+emailInput.onkeyup = () => {
+  clearValidationStyles(emailInput);
+};
+
+passwordInput.onkeyup = () => {
+  clearValidationStyles(passwordInput);
+};
+
+confirmPasswordInput.onkeyup = () => {
+  clearValidationStyles(confirmPasswordInput);
+};
+
 
 // add callback function for submit button.
 submitBtn.onclick = () => {
